f0c707fc1b44ca8d530643da1e702a397ad365bb
Ash <ash@burnt.com>
2023-03-31 17:24:00 +0100

    protos fixed

--- x/mint/module.go
+++ x/mint/module.go
@@ -31,7 +31,7 @@ import (
 )
 
 // ConsensusVersion defines the current x/mint module consensus version.
-const ConsensusVersion = 2
+const ConsensusVersion = 1
 
 var (
 	_ module.BeginBlockAppModule = AppModule{}
@@ -150,12 +150,6 @@ func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
-
-	m := keeper.NewMigrator(am.keeper, am.legacySubspace)
-
-	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
-		panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", types.ModuleName, err))
-	}
 }
 
 // InitGenesis performs genesis initialization for the mint module. It returns
