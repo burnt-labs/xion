503c375af23aae1f1eb1b77406c0da91a0cb858f
peartes <kenny.fale.kf@gmail.com>
2023-05-19 13:10:45 +0100

    chore: added code comments

--- integration_tests/utils.go
+++ integration_tests/utils.go
@@ -23,6 +23,7 @@ import (
 	"go.uber.org/zap/zaptest"
 )
 
+// Function type for any function that modify the genesis file
 type ModifyInterChainGenesisFn []func(ibc.ChainConfig, []byte, ...string) ([]byte, error)
 
 func BuildXionChain(t *testing.T, modifyGenesis func(ibc.ChainConfig, []byte) ([]byte, error)) (*cosmos.CosmosChain, context.Context) {
@@ -95,6 +96,11 @@ func BuildXionChain(t *testing.T, modifyGenesis func(ibc.ChainConfig, []byte) ([
 	return xion, ctx
 }
 
+/*
+ * This function is a helper to run all functions that modify the genesis file
+ * in a chain. It takes a list of functions of the type ModifyInterChainGenesisFn and a list of list of parameters for each
+ * function. Each array in the parameter list are the parameters for a functions of the same index
+ */
 func ModifyInterChainGenesis(fns ModifyInterChainGenesisFn, params [][]string) func(ibc.ChainConfig, []byte) ([]byte, error) {
 	return func(chainConfig ibc.ChainConfig, genbz []byte) ([]byte, error) {
 		res := genbz
@@ -110,6 +116,7 @@ func ModifyInterChainGenesis(fns ModifyInterChainGenesisFn, params [][]string) f
 	}
 }
 
+// This function modifies the proposal parameters of the gov module in the genesis file
 func ModifyGenesisShortProposals(chainConfig ibc.ChainConfig, genbz []byte, params ...string) ([]byte, error) {
 	g := make(map[string]interface{})
 	if err := json.Unmarshal(genbz, &g); err != nil {
@@ -134,6 +141,7 @@ func ModifyGenesisShortProposals(chainConfig ibc.ChainConfig, genbz []byte, para
 	return out, nil
 }
 
+// This function modifies the inflation parameters of the mint module in the genesis file
 func ModifyGenesisInflation(chainConfig ibc.ChainConfig, genbz []byte, params ...string) ([]byte, error) {
 	g := make(map[string]interface{})
 	if err := json.Unmarshal(genbz, &g); err != nil {
@@ -155,18 +163,25 @@ func ModifyGenesisInflation(chainConfig ibc.ChainConfig, genbz []byte, params ..
 	return out, nil
 }
 
+// Helper method to retrieve the total token supply for a chain at some particular history denoted by the block height
 func getTotalCoinSupplyInBank(t *testing.T, xion *cosmos.CosmosChain, ctx context.Context, denom string, blockHeight uint64) string {
 	if blockHeight == 0 {
+		// No history is required so use the most recent block height
 		blockHeight, _ = xion.Height(ctx)
 		require.Greater(t, blockHeight, 0)
 	}
+
+	/*
+	 * Response is of the structure
+	 * {"supply":[{"denom":"uxion","amount":"110000002059725"}]}
+	 */
 	jsonRes := make(map[string]interface{})
 	queryRes, _, err := xion.FullNodes[0].ExecQuery(ctx, "bank", "total", "--height", strconv.FormatInt(int64(blockHeight), 10))
 	require.NoError(t, err)
 
 	require.NoError(t, json.Unmarshal(queryRes, &jsonRes))
 
-	// Presuming we are the only denom on the chain
+	// Presuming we are the only denom on the chain then the returned array only has one Coin type ours (uxion)
 	totalSupply, err := dyno.GetSlice(jsonRes, "supply")
 	require.NoError(t, err)
 	xionCoin := totalSupply[0]
@@ -175,16 +190,23 @@ func getTotalCoinSupplyInBank(t *testing.T, xion *cosmos.CosmosChain, ctx contex
 	xionCoinDenom, err := dyno.GetString(xionCoin, "denom")
 	require.NoError(t, err)
 	require.Equal(t, xionCoinDenom, denom)
+	// Get the returned amount
 	initialXionSupply, err := dyno.GetString(xionCoin, "amount")
 	require.NoError(t, err)
 	return initialXionSupply
 }
 
+// This function gets the bank balance for an address at some particular history denoted by the block height
 func getAddressBankBalanceAtHeight(t *testing.T, xion *cosmos.CosmosChain, ctx context.Context, address string, denom string, blockHeight uint64) string {
 	if blockHeight == 0 {
 		blockHeight, _ = xion.Height(ctx)
 		require.Greater(t, blockHeight, 0)
 	}
+
+	/*
+	 * Response is of the structure
+	 * {"supply":[{"denom":"uxion","amount":"110000002059725"}]}
+	 */
 	jsonRes := make(map[string]interface{})
 	queryRes, _, err := xion.FullNodes[0].ExecQuery(ctx, "bank", "balances", address, "--height", strconv.FormatInt(int64(blockHeight), 10))
 	require.NoError(t, err)
@@ -206,7 +228,24 @@ func getAddressBankBalanceAtHeight(t *testing.T, xion *cosmos.CosmosChain, ctx c
 	return balance
 }
 
+// This function gets the module address for a module name
 func GetModuleAddress(t *testing.T, xion *cosmos.CosmosChain, ctx context.Context, moduleName string) string {
+	/*
+		* Response is of the type
+		* {
+			"account": {
+				"@type": "/cosmos.auth.v1beta1.ModuleAccount",
+				"base_account": {
+				"address": "xion1jv65s3grqf6v6jl3dp4t6c9t9rk99cd89k77l5",
+				"pub_key": null,
+				"account_number": "3",
+				"sequence": "0"
+				},
+				"name": "distribution",
+				"permissions": []
+			}
+			}
+	*/
 	jsonRes := make(map[string]interface{})
 	queryRes, _, err := xion.FullNodes[0].ExecQuery(ctx, "auth", "module-account", moduleName)
 	require.NoError(t, err)
@@ -219,7 +258,7 @@ func GetModuleAddress(t *testing.T, xion *cosmos.CosmosChain, ctx context.Contex
 	return moduleAddress
 }
 
-// Retrieve a block annual provision
+// Retrieve a block annual provision. This is the minted tokens for the block for validators and delegator
 func GetBlockAnnualProvision(t *testing.T, xion *cosmos.CosmosChain, ctx context.Context, denom string, blockHeight uint64) math.LegacyDec {
 	if blockHeight == 0 {
 		blockHeight, _ = xion.Height(ctx)
@@ -227,6 +266,7 @@ func GetBlockAnnualProvision(t *testing.T, xion *cosmos.CosmosChain, ctx context
 	}
 
 	// Query the current block provision
+	// Response is a string
 	var annualProvision json.Number
 	queryRes, _, err := xion.FullNodes[0].ExecQuery(ctx, "mint", "annual-provisions", "--height", strconv.FormatInt(int64(blockHeight), 10))
 	require.NoError(t, err)
@@ -239,11 +279,29 @@ func GetBlockAnnualProvision(t *testing.T, xion *cosmos.CosmosChain, ctx context
 	blocksPerYear, err := dyno.GetInteger(params, "blocks_per_year")
 	require.NoError(t, err)
 	// Calculate the block provision
-	return math.LegacyMustNewDecFromStr(annualProvision.String()).QuoInt(math.NewInt(blocksPerYear)) // This ideally is the minted tokens for the block
+	return math.LegacyMustNewDecFromStr(annualProvision.String()).QuoInt(math.NewInt(blocksPerYear))
 }
 
 // This test confirms the property of the module described at
 // https://www.notion.so/burntlabs/Mint-Module-Blog-Post-78f59fb108c04e9ea5fa826dda30a340
+/*
+ * The mint harness test logic is as follows
+ * Given a particular block height, we compute the minted or burned tokens at that block using the following algorithm
+
+ * Get the total token supply at the previous height
+ * Get the total token supply at the current height
+ * Compute the change in the total supply (d)
+ * Get the balance of the distribution module account in the previous block
+ * Get the balance of the distribution module account at the current block
+ * The difference (f) is the sum of the total fees and minted token at the current block
+ * The difference ( d - f )is the total fees accrued at the current block
+ * Get the block provision at the current height (this is the number of tokens to be minted for validators and delegators at that block
+ * Perform the following checks
+ * If we have more fees accrued than the block provision, we burn the excess tokens
+ * If instead there are fewer fees than block provision, we only mint (provision - fees) tokens to meet expectations
+ * Otherwise, we had equal fees and provisions. We do not mint or burn any tokens
+ * If these checks passes then the mint module functions as expected
+ */
 func MintModuleTestHarness(t *testing.T, xion *cosmos.CosmosChain, ctx context.Context, blockHeight int) {
 	// Get bank supply at previous height
 	previousXionBankSupply, err := strconv.ParseInt(getTotalCoinSupplyInBank(t, xion, ctx, xion.Config().Denom, uint64(blockHeight-1)), 10, 64)
@@ -266,31 +324,34 @@ func MintModuleTestHarness(t *testing.T, xion *cosmos.CosmosChain, ctx context.C
 
 	delta := currentDistributionModuleBalance - previousDistributionModuleBalance
 
+	// Fees Accrued is the total fees in a block. Since the tokens in the distribution module account
+	// are the fees and the minted tokens, we can compute the fees accrued by subtracting the token change
+	// from the delta
 	feesAccrued := delta - tokenChange
 	t.Logf("Fees accrued: %d", feesAccrued)
 
-	// Get the block provision
+	// Get the block provision. This is the minted tokens for the block for validators and delegator
 	blockProvision := GetBlockAnnualProvision(t, xion, ctx, xion.Config().Denom, uint64(blockHeight))
 
-	// Make sure the minted tokens is equal to the block provision - fees accrued
 	if blockProvision.TruncateInt().GT(math.NewInt(feesAccrued)) {
-		// We have minted tokens
+		// We have minted tokens because the fees accrued is less than the block provision
 		mintedTokens := blockProvision.TruncateInt().Sub(math.NewInt(feesAccrued))
 		t.Logf("Minted tokens: %d and Token change: %d", mintedTokens.Int64(), int64(tokenChange))
 		require.Equal(t, mintedTokens, math.NewInt(int64(tokenChange)))
 	} else if blockProvision.TruncateInt().LT(math.NewInt(feesAccrued)) {
-		// We have burned tokens
+		// We have burned tokens because the fees accrued is greater than the block provision so the fees
+		// accrued are used to pay validators and the remaining is burned
 		burnedTokens := math.NewInt(feesAccrued).Sub(blockProvision.TruncateInt())
 		t.Logf("Burned tokens: %d and Token change: %d", burnedTokens.Int64(), tokenChange)
 		require.Equal(t, burnedTokens, math.NewInt(tokenChange).Abs())
 	} else {
-		// We have not minted or burned tokens
+		// We have not minted or burned tokens but just used all fees accrued to pay validators
 		require.Equal(t, math.NewInt(0), math.NewInt(tokenChange))
 		t.Logf("No minted or Burned tokens. Token change: %d", tokenChange)
 	}
 }
 
-// Run Mint module test over some random block height
+// Run Mint module test harness over some random block height
 // Chain must have at least 12 blocks
 func VerifyMintModuleTestRandomBlocks(t *testing.T, xion *cosmos.CosmosChain, ctx context.Context) {
 
@@ -298,7 +359,7 @@ func VerifyMintModuleTestRandomBlocks(t *testing.T, xion *cosmos.CosmosChain, ct
 	require.NoError(t, err)
 	require.GreaterOrEqual(t, currentBlockHeight, uint64(12))
 	// Get a random number from 1 to the (currentBlockHeight - 10)
-	randomHeight := rand.Intn(int(currentBlockHeight)-11) + 2
+	randomHeight := rand.Intn(int(currentBlockHeight)-11) + 2 // we start from 2 because we need at least 2 blocks to run the test
 
 	for i := randomHeight; i < randomHeight+10; i++ {
 		t.Logf("Current random height: %d", i)
