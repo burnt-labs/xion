df8009f589482948c6abc6cfb9adf0e8d3a8da70
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2023-06-03 21:00:39 -0400

    remove migrations from module

--- x/globalfee/module.go
+++ x/globalfee/module.go
@@ -3,7 +3,6 @@ package globalfee
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 
 	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -18,7 +17,6 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/burnt-labs/xion/x/globalfee/client/cli"
-	"github.com/burnt-labs/xion/x/globalfee/keeper"
 	"github.com/burnt-labs/xion/x/globalfee/types"
 )
 
@@ -109,25 +107,12 @@ func (a AppModule) ExportGenesis(ctx sdk.Context, marshaler codec.JSONCodec) jso
 func (a AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {
 }
 
-func (a AppModule) Route() sdk.Route {
-	return sdk.Route{}
-}
-
 func (a AppModule) QuerierRoute() string {
 	return types.QuerierRoute
 }
 
-func (a AppModule) LegacyQuerierHandler(_ *codec.LegacyAmino) sdk.Querier {
-	return nil
-}
-
 func (a AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterQueryServer(cfg.QueryServer(), NewGrpcQuerier(a.paramSpace))
-
-	m := keeper.NewMigrator(a.paramSpace)
-	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
-		panic(fmt.Sprintf("failed to migrate x/globalfee from version 1 to 2: %v", err))
-	}
 }
 
 func (a AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
