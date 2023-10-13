fbfbce469db96a9a87e54ee42dcb357523c84db5
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2023-10-12 21:16:14 -0400

    WIP; aa fix; test outline

--- integration_tests/abstract_account_test.go
+++ integration_tests/abstract_account_test.go
@@ -9,6 +9,10 @@ import (
 	ibctest "github.com/strangelove-ventures/interchaintest/v7"
 	"github.com/strangelove-ventures/interchaintest/v7/testutil"
 	"github.com/stretchr/testify/require"
+
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	aatypes "github.com/larry0x/abstract-account/x/abstractaccount/types"
 )
 
 func TestXionAbstractAccount(t *testing.T) {
@@ -20,6 +24,16 @@ func TestXionAbstractAccount(t *testing.T) {
 
 	td := BuildXionChain(t, "0.0uxion", ModifyInterChainGenesis(ModifyInterChainGenesisFn{ModifyGenesisShortProposals}, [][]string{{votingPeriod, maxDepositPeriod}}))
 	xion, ctx := td.xionChain, td.ctx
+
+	xion.Config().EncodingConfig.InterfaceRegistry.RegisterImplementations(
+		(*types.Msg)(nil),
+		&xiontypes.MsgSetPlatformPercentage{},
+		&xiontypes.MsgSend{},
+		&wasmtypes.MsgInstantiateContract{},
+		&wasmtypes.MsgStoreCode{},
+		&aatypes.MsgUpdateParams{},
+		&aatypes.MsgRegisterAccount{},
+	)
 
 	// Create and Fund User Wallets
 	t.Log("creating and funding user accounts")
@@ -38,41 +52,55 @@ func TestXionAbstractAccount(t *testing.T) {
 	recipientKeyName := "recipient-key"
 	err = xion.CreateKey(ctx, recipientKeyName)
 	require.NoError(t, err)
-	receipientKeyAddressBytes, err := xion.GetAddress(ctx, recipientKeyName)
-	require.NoError(t, err)
-	recipientKeyAddress, err := types.Bech32ifyAddressBytes(xion.Config().Bech32Prefix, receipientKeyAddressBytes)
-	require.NoError(t, err)
-
-	xion.Config().EncodingConfig.InterfaceRegistry.RegisterImplementations(
-		(*types.Msg)(nil),
-		&xiontypes.MsgSetPlatformPercentage{},
-		&xiontypes.MsgSend{},
-	)
-
-	currentHeight, _ = xion.Height(ctx)
-	_, err = ExecTx(t, ctx, xion.FullNodes[0],
-		xionUser.KeyName(),
-		"xion", "send", xionUser.KeyName(),
-		"--chain-id", xion.Config().ChainID,
-		recipientKeyAddress, fmt.Sprintf("%d%s", 100, xion.Config().Denom),
-	)
-
-	codeID, err := xion.StoreContract(ctx, xionUser.FormattedAddress(), "../testdata/contracts/account_updatable-aarch64.wasm")
-	require.NoError(t, err)
-	_, err = xion.InstantiateContract(ctx, xionUser.FormattedAddress(), codeID, "", false)
-	require.NoError(t, err)
 
 	/*
-		_, err = ExecTx(t, ctx, xion.FullNodes[0],
-			xionUser.KeyName(),
-			"xion", "wasm", "store", "[path_to_file]", xionUser.KeyName(),
-			"--chain-id", xion.Config().ChainID,
-			recipientKeyAddress, fmt.Sprintf("%d%s", 100, xion.Config().Denom),
-		)
+		receipientKeyAddressBytes, err := xion.GetAddress(ctx, recipientKeyName)
+		require.NoError(t, err)
+		recipientKeyAddress, err := types.Bech32ifyAddressBytes(xion.Config().Bech32Prefix, receipientKeyAddressBytes)
+		require.NoError(t, err)
 	*/
 
+	currentHeight, _ = xion.Height(ctx)
+	account, err := ExecBin(t, ctx, xion.FullNodes[0],
+		xionUser.KeyName(),
+		"keys", "show",
+		xionUser.KeyName(),
+		"--keyring-backend", keyring.BackendTest,
+		"-p",
+	)
 	require.NoError(t, err)
-	balance, err := xion.GetBalance(ctx, recipientKeyAddress, xion.Config().Denom)
+	fmt.Println("Post Query")
+	fmt.Println("=======================================================")
+	fmt.Println(account)
+	fmt.Println("=======================================================")
+
+	codeID, err := xion.StoreContract(ctx, xionUser.FormattedAddress(), "./testdata/contracts/account_updatable-aarch64.wasm")
 	require.NoError(t, err)
-	require.Equal(t, uint64(100), uint64(balance))
+
+	/* add register AA using Public key */
+	registeredTxHash, err := ExecTx(t, ctx, xion.FullNodes[0],
+		xionUser.KeyName(),
+		"abstract-account", "register",
+		codeID,
+		fmt.Sprintf(`{"pubkey": "%s"}`, account["key"]),
+		"--funds", fmt.Sprintf("%d%s", 100000, xion.Config().Denom),
+		"--salt", "foo",
+		"--chain-id", xion.Config().ChainID,
+	)
+
+	txDetails, err := ExecQuery(t, ctx, xion.FullNodes[0], "tx", registeredTxHash)
+	require.NoError(t, err)
+	fmt.Println("Post Register?")
+	fmt.Println("=======================================================")
+	fmt.Println(r)
+	fmt.Println("=======================================================")
+
+	//_, err = xion.InstantiateContract(ctx, xionUser.FormattedAddress(), codeID, fmt.Sprintf(`{"new_pubkey:%s"}`, ""), true)
+
+	/*
+		require.NoError(t, err)
+		balance, err := xion.GetBalance(ctx, recipientKeyAddress, xion.Config().Denom)
+		require.NoError(t, err)
+		require.Equal(t, uint64(100), uint64(balance))
+	*/
 }
