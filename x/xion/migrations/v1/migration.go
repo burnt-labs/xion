e46543ce780943a999d53ddb8d9e55cd4ad8b57e
Ash <ash@burnt.com>
2024-02-19 07:55:16 -0700

    pull code IDs from params

--- x/xion/migrations/v1/migration.go
+++ x/xion/migrations/v1/migration.go
@@ -1,23 +1,34 @@
 package v1
 
 import (
+	"fmt"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/burnt-labs/xion/x/xion/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func MigrateStore(ctx sdk.Context, wasmOpsKeeper wasmtypes.ContractOpsKeeper, wasmViewKeeper wasmtypes.ViewKeeper, aaKeeper types.AbstractAccountKeeper) error {
-	const OriginalCodeId = 21
 	const NewCodeId = 21 // todo: set
 
+	// get the previous account code ID
+	aaParams, err := aaKeeper.GetParams(ctx)
+	if err != nil {
+		return err
+	}
+	if len(aaParams.AllowedCodeIDs) != 1 {
+		return fmt.Errorf("expected one allowed code id for abstract account")
+	}
+
+	originalCodeId := aaParams.AllowedCodeIDs[0]
+
 	// the account contract should always be pinned
-	err := wasmOpsKeeper.PinCode(ctx, NewCodeId)
+	err = wasmOpsKeeper.PinCode(ctx, NewCodeId)
 	if err != nil {
 		return err
 	}
 
 	// iterate through all existing accounts at this code ID, and migrate them
-	wasmViewKeeper.IterateContractsByCode(ctx, OriginalCodeId, func(instance sdk.AccAddress) bool {
+	wasmViewKeeper.IterateContractsByCode(ctx, originalCodeId, func(instance sdk.AccAddress) bool {
 		_, err = wasmOpsKeeper.Migrate(ctx, instance, instance, NewCodeId, nil)
 
 		// if there is an error, abort iteration and report it
@@ -29,16 +40,12 @@ func MigrateStore(ctx sdk.Context, wasmOpsKeeper wasmtypes.ContractOpsKeeper, wa
 
 	// as the previous contract is no longer the main account target, it doesn't
 	// need to be pinned
-	err = wasmOpsKeeper.UnpinCode(ctx, OriginalCodeId)
+	err = wasmOpsKeeper.UnpinCode(ctx, originalCodeId)
 	if err != nil {
 		return err
 	}
 
 	// adjust the aa registration endpoint to point at the new code ID
-	aaParams, err := aaKeeper.GetParams(ctx)
-	if err != nil {
-		return err
-	}
 	aaParams.AllowedCodeIDs = []uint64{NewCodeId}
 	err = aaKeeper.SetParams(ctx, aaParams)
 	if err != nil {
