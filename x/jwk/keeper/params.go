ec89312533930123c6a89c85daf2933bc73ee952
Ash <ash@burnt.com>
2024-03-21 11:25:28 -0700

    get params was returning empty params

--- x/jwk/keeper/params.go
+++ x/jwk/keeper/params.go
@@ -7,7 +7,9 @@ import (
 
 // GetParams get all parameters as types.Params
 func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	return types.NewParams()
+	ps := types.Params{}
+	k.paramstore.GetParamSet(ctx, &ps)
+	return ps
 }
 
 // SetParams set the params
