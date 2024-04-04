4f9e41a67c05a0df8d83f8c4f4c52ea0973639d1
peartes <kenny.fale.kf@gmail.com>
2024-04-04 16:28:19 +0100

    chore: [WIP] integration test

---
+++ integration_tests/authz_test.go
@@ -0,0 +1,233 @@
+package integration_tests
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	xionapp "github.com/burnt-labs/xion/app"
+	xiontypes "github.com/burnt-labs/xion/x/xion/types"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	aatypes "github.com/larry0x/abstract-account/x/abstractaccount/types"
+	ibctest "github.com/strangelove-ventures/interchaintest/v7"
+	"github.com/strangelove-ventures/interchaintest/v7/ibc"
+	"github.com/strangelove-ventures/interchaintest/v7/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func setupChainForAuth(t *testing.T) (TestData, ibc.Wallet, []byte, string, error) {
+	td := BuildXionChain(t, "0.0uxion", ModifyInterChainGenesis(ModifyInterChainGenesisFn{ModifyGenesisShortProposals}, [][]string{{votingPeriod, maxDepositPeriod}}))
+	xion, ctx := td.xionChain, td.ctx
+
+	config := types.GetConfig()
+	config.SetBech32PrefixForAccount("xion", "xionpub")
+
+	// Create and Fund User Wallets
+	t.Log("creating and funding user accounts")
+	fundAmount := int64(10_000_000)
+	// users := ibctest.GetAndFundTestUsers(t, ctx, "default", fundAmount, xion)
+	deployerAddr, err := ibctest.GetAndFundTestUserWithMnemonic(ctx, "default", deployerMnemonic, fundAmount, xion)
+	require.NoError(t, err)
+	err = testutil.WaitForBlocks(ctx, 8, xion)
+	require.NoError(t, err)
+	t.Logf("created xion user %s", deployerAddr.FormattedAddress())
+
+	xionUserBalInitial, err := xion.GetBalance(ctx, deployerAddr.FormattedAddress(), xion.Config().Denom)
+	require.NoError(t, err)
+	require.Equal(t, fundAmount, xionUserBalInitial)
+
+	// register any needed msg types
+	xion.Config().EncodingConfig.InterfaceRegistry.RegisterImplementations(
+		(*types.Msg)(nil),
+		&xiontypes.MsgSetPlatformPercentage{},
+		&xiontypes.MsgSend{},
+		&wasmtypes.MsgInstantiateContract{},
+		&wasmtypes.MsgStoreCode{},
+		&aatypes.MsgUpdateParams{},
+		&aatypes.MsgRegisterAccount{},
+	)
+	xion.Config().EncodingConfig.InterfaceRegistry.RegisterImplementations((*authtypes.AccountI)(nil), &aatypes.AbstractAccount{})
+	xion.Config().EncodingConfig.InterfaceRegistry.RegisterImplementations((*cryptotypes.PubKey)(nil), &aatypes.NilPubKey{})
+	// t.Log(xion.Config().EncodingConfig.InterfaceRegistry.ListImplementations("/xion.v1.Msg/Send"))
+	fp, err := os.Getwd()
+	require.NoError(t, err)
+
+	// deploy the contract
+	codeIDStr, err := xion.StoreContract(ctx, deployerAddr.FormattedAddress(),
+		path.Join(fp, "testdata", "contracts", "simple_contract-aarch64.wasm"))
+	require.NoError(t, err)
+
+	// retrieve the hash
+	codeResp, err := ExecQuery(t, ctx, xion.FullNodes[0],
+		"wasm", "code-info", codeIDStr)
+	require.NoError(t, err)
+	t.Logf("code response: %s", codeResp)
+
+	codeHash, _ := hex.DecodeString(codeResp["data_hash"].(string))
+
+	return td, deployerAddr, codeHash, codeIDStr, nil
+}
+func TestXionCodeIdAuth(t *testing.T) {
+	t.Parallel()
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
+	td, deployerAddr, _, codeIDStr, err := setupChainForAuth(t)
+	require.NoError(t, err)
+
+	xion, ctx := td.xionChain, td.ctx
+
+	require.NoError(t, err)
+
+	instantiateCmd := []string{
+		"wasm", "instantiate",
+		codeIDStr, "{}",
+		"--chain-id", xion.Config().ChainID,
+		"--label", "test", "--no-admin",
+	}
+	t.Logf("sender: %s", deployerAddr.FormattedAddress())
+
+	txHash, err := ExecTx(t, ctx, xion.FullNodes[0], deployerAddr.KeyName(), instantiateCmd...)
+	require.NoError(t, err)
+	t.Logf("tx hash: %s", txHash)
+
+	contractsResponse, err := ExecQuery(t, ctx, xion.FullNodes[0], "wasm", "contracts", codeIDStr)
+	require.NoError(t, err)
+
+	contract := contractsResponse["contracts"].([]interface{})[0].(string)
+
+	// get the account from the chain. there might be a better way to do this
+	accountResponse, err := ExecQuery(t, ctx, xion.FullNodes[0],
+		"account", contract)
+	require.NoError(t, err)
+	t.Logf("account response: %s", accountResponse)
+
+	delete(accountResponse, "@type")
+	var account aatypes.AbstractAccount
+	accountJSON, err := json.Marshal(accountResponse)
+	require.NoError(t, err)
+
+	encodingConfig := xionapp.MakeEncodingConfig()
+	err = encodingConfig.Marshaler.UnmarshalJSON(accountJSON, &account)
+	require.NoError(t, err)
+
+	err = xion.SendFunds(ctx, deployerAddr.FormattedAddress(), ibc.WalletAmount{Address: contract, Denom: "uxion", Amount: 10_000})
+	require.NoError(t, err)
+	// create the raw tx
+	sendMsg := fmt.Sprintf(`
+	{
+		"body": {
+		  "messages": [
+			{
+			  "@type": "/cosmwasm.wasm.v1.MsgExecuteContract",
+			  "sender": "%s",
+			  "contract": "%s",
+			  "msg": {},
+			  "funds": []
+			}
+		  ],
+		  "memo": "",
+		  "timeout_height": "0",
+		  "extension_options": [],
+		  "non_critical_extension_options": []
+		},
+		"auth_info": {
+		  "signer_infos": [],
+		  "fee": {
+			"amount": [],
+			"gas_limit": "200000",
+			"payer": "",
+			"granter": ""
+		  },
+		  "tip": null
+		},
+		"signatures": []
+	  }
+		`, deployerAddr.FormattedAddress(), account.Address)
+
+	tx, err := encodingConfig.TxConfig.TxJSONDecoder()([]byte(sendMsg))
+	require.NoError(t, err)
+	txBuilder, err := encodingConfig.TxConfig.WrapTxBuilder(tx)
+	require.NoError(t, err)
+	// create the sign bytes
+	signerData := authsigning.SignerData{
+		Address:       account.GetAddress().String(),
+		ChainID:       xion.Config().ChainID,
+		AccountNumber: account.GetAccountNumber(),
+		Sequence:      account.GetSequence(),
+		PubKey:        account.GetPubKey(),
+	}
+
+	sigData := signing.SingleSignatureData{
+		SignMode:  signing.SignMode_SIGN_MODE_DIRECT,
+		Signature: nil,
+	}
+
+	sig := signing.SignatureV2{
+		PubKey:   account.GetPubKey(),
+		Data:     &sigData,
+		Sequence: account.GetSequence(),
+	}
+
+	err = txBuilder.SetSignatures(sig)
+	require.NoError(t, err)
+
+	signBytes, err := encodingConfig.TxConfig.SignModeHandler().GetSignBytes(signing.SignMode_SIGN_MODE_DIRECT, signerData, txBuilder.GetTx())
+	require.NoError(t, err)
+	// our signature is the sha256 of the signbytes
+	signatureBz := sha256.Sum256(signBytes)
+	challenge := base64.StdEncoding.EncodeToString(signatureBz[:])
+
+	t.Log("challenge ", challenge)
+
+	/*
+		The following is a valid signed challenge from a webauthn authenticator the using above url. This will need to be updated from time to time when the accounts contract is updated.
+		To regenerate, use the following steps:
+		1. Run this test and make note of the `challenge` output above.
+		2. Go to the url above and open the developer tools console.
+		3. Enter the `challenge` in the first field within the WebAuthN section (populated with the string "test-challenge" by default)
+		4. Click "Sign" and copy the result from the console
+	*/
+	signedChallenge := `{"type":"public-key","id":"UWxY-yRdIls8IT-vyMS6la1ZiqESOAff7bWZ_LWV0Pg","rawId":"UWxY-yRdIls8IT-vyMS6la1ZiqESOAff7bWZ_LWV0Pg","authenticatorAttachment":"platform","response":{"clientDataJSON":"eyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoiT1V0UGMyMWpMM3BhWVROd2NFbEJXSEZMYjNkTk1sTmxjR1JRWWs5a09GUlJOMEp6ZGk4elRYb3laejAiLCJvcmlnaW4iOiJodHRwczovL3hpb24tZGFwcC1leGFtcGxlLWdpdC1mZWF0LWZhY2VpZC1idXJudGZpbmFuY2UudmVyY2VsLmFwcCIsImNyb3NzT3JpZ2luIjpmYWxzZX0","authenticatorData":"sGMBiDcEppiMfxQ10TPCe2-FaKrLeTkvpzxczngTMw0FAAAAAA","signature":"MEQCIE0N5DESKA8PeykB1t98815g99XeXpxxZ94UjDuSl85pAiBssBSfcsYJ4c4hxkLoaVj635pBuGI3IBPBia8VJ4HOug","userHandle":"eGlvbjFuY3gwYTBqbnN5YXk3dWRkMDNhaDJnZjY0NzcyZzAycXN3ajUyOTk2ZHk4MHFmdmdubXpxNmVwbHFx"},"clientExtensionResults":{}}`
+	// add the auth index to the signature
+	signedTokenBz := []byte(signedChallenge)
+	sigBytes := append([]byte{0}, signedTokenBz...)
+
+	sigData = signing.SingleSignatureData{
+		SignMode:  signing.SignMode_SIGN_MODE_DIRECT,
+		Signature: sigBytes,
+	}
+
+	sig = signing.SignatureV2{
+		PubKey:   account.GetPubKey(),
+		Data:     &sigData,
+		Sequence: account.GetSequence(),
+	}
+	err = txBuilder.SetSignatures(sig)
+	require.NoError(t, err)
+
+	jsonTx, err := encodingConfig.TxConfig.TxJSONEncoder()(txBuilder.GetTx())
+	require.NoError(t, err)
+	t.Logf("json tx: %s", jsonTx)
+
+	output, err := ExecBroadcast(t, ctx, xion.FullNodes[0], jsonTx)
+	require.NoError(t, err)
+	t.Logf("output: %s", output)
+
+	err = testutil.WaitForBlocks(ctx, 2, xion)
+	require.NoError(t, err)
+	newBalance, err := xion.GetBalance(ctx, contract, xion.Config().Denom)
+	require.NoError(t, err)
+	require.Equal(t, int64(10_000-1337), newBalance)
+}
