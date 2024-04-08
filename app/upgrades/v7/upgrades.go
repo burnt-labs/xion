8fe3753ef2c803ec9e19d953ae8f20a69757e6ca
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2024-04-08 12:11:45 -0400

    [WIP] v7 upgrade

---
+++ app/upgrades/v7/upgrades.go
@@ -0,0 +1,30 @@
+package v7
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+/*
+const NewDenomCreationGasConsume uint64 = 2_000_000
+
+var DenomCreationFee = sdk.NewCoins(sdk.NewInt64Coin(`uxion`, 50_000_000))
+*/
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("Starting module migrations...")
+
+		vm, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return vm, err
+		}
+
+		ctx.Logger().Info("Upgrade complete")
+		return vm, err
+	}
+}
