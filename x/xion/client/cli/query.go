736773b38baaad1e782ae805f26fc6099ff601cf
Ash <ash@burnt.com>
2024-05-02 13:02:44 -0400

    query commands for webauthn

---
+++ x/xion/client/cli/query.go
@@ -0,0 +1,30 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+
+	"github.com/burnt-labs/xion/x/xion/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd() *cobra.Command {
+	// Group jwk queries under a subcommand
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(CmdWebAuthNVerifyRegister())
+	cmd.AddCommand(CmdWebAuthNVerifyAuthenticate())
+
+	// this line is used by starport scaffolding # 1
+
+	return cmd
+}
