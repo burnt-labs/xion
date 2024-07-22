34f55930f6c52553b3c33dcd3d46a485203b95fc
2xburnt <169301814+2xburnt@users.noreply.github.com>
2024-07-22 12:31:52 -0500

    WIP

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -2,18 +2,46 @@
 
 set -eo pipefail
 
-mkdir -p ./docs/client
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
+# change to the root folder
+cd "$(dirname $(realpath "$0"))/.."
 
+mkdir -p ./tmp-swagger-gen
+
+# Get the path of the cosmos-sdk repo from go/pkg/mod
+proto_paths=$(go list -f '{{ .Dir }}' -m \
+  github.com/gogo/protobuf \
+  github.com/cosmos/cosmos-sdk \
+  github.com/cosmos/ibc-go/v7 \
+  github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v7 \
+  github.com/CosmWasm/wasmd \
+  github.com/cosmos/cosmos-proto \
+  | sed "s/$/\/proto/"
+)
+
+proto_dirs=$(
+  printf "./proto\n$proto_paths" \
+  | while read path; do
+    find $path -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname
+  done | sort -u
+)
+
+set -x
+printf "$proto_dirs" | while read dir; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+
   if [[ ! -z "$query_file" ]]; then
-    buf protoc  \
-    -I "proto" \
-    -I "third_party/proto" \
-    "$query_file" \
-    --swagger_out=./docs/client \
-    --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
+    #buf generate --template proto/buf.gen.swagger.yaml $query_file
+    protoc -I ./proto $(printf "$proto_paths" | sed 's/^/ -I /') $query_file \
+    --swagger_out ./tmp-swagger-gen \
+    --swagger_opt logtostderr=true \
+    --swagger_opt fqn_for_swagger_name=true \
+    --swagger_opt simple_operation_ids=true
   fi
 done
+
+npm install -g swagger-combine
+npx swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+
+# clean swagger files
+rm -rf ./tmp-swagger-gen
