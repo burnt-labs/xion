934ac3439f7f291688349a96f66173bb032f1ae1
Francis Roch <francis@manafont.net>
2024-08-08 21:42:15 -0400

    Upgrade v10

---
+++ app/upgrades/v10/constants.go
@@ -0,0 +1,18 @@
+package v9
+
+import (
+	store "cosmossdk.io/store/types"
+
+	"github.com/burnt-labs/xion/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v10"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades:        store.StoreUpgrades{},
+}
---
+++ app/upgrades/v10/upgrades.go
@@ -0,0 +1,29 @@
+package v9
+
+import (
+	"context"
+	"fmt"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info("Starting module migrations...")
+
+		vm, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return vm, err
+		}
+
+		sdkCtx.Logger().Info(fmt.Sprintf("Software Upgrade %s complete", UpgradeName))
+		return vm, err
+	}
+}
