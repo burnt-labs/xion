b76b9493548a288ec48e34834a9cbfbe8be46e98
Francis Roch <francis@manafont.net>
2024-08-12 19:33:04 -0400

    v10 minion tests

--- integration_tests/upgrade_v10_minion_test.go
+++ integration_tests/upgrade_v10_minion_test.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"testing"
 
-	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	dbm "github.com/cosmos/cosmos-db"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	"github.com/stretchr/testify/require"
@@ -13,19 +12,14 @@ import (
 	"github.com/burnt-labs/xion/app"
 )
 
-var emptyWasmOpts []wasmkeeper.Option
-
 func TestUpgradeV10Minion(t *testing.T) {
 	// Create a new instance of the app
 	db := dbm.NewMemDB()
-	simApp := app.NewWasmApp(
-		log.NewLogger(os.Stdout),
-		db,
-		nil,
-		true,
-		simtestutil.NewAppOptionsWithFlagHome(t.TempDir()),
-		emptyWasmOpts,
-	)
+	simApp := app.NewWasmAppWithCustomOptions(t, false, app.SetupOptions{
+		Logger:  log.NewLogger(os.Stdout),
+		DB:      db,
+		AppOpts: simtestutil.NewAppOptionsWithFlagHome(t.TempDir()),
+	})
 
 	require.IsType(t, &app.WasmApp{}, simApp)
 }
