0ae43e236e57d6a0e6d25fe2bb153751da0f9193
2xburnt <169301814+2xburnt@users.noreply.github.com>
2024-08-13 13:25:19 -0500

    fix deps in protoc-common.sh

--- scripts/protoc-common.sh
+++ scripts/protoc-common.sh
@@ -10,12 +10,18 @@ set -eo pipefail
 : ${base_dir:="$(dirname $scripts_dir)"}
 : ${proto_dir:="$base_dir/proto"}
 
-# # Define dependencies
+# Define dependencies
+# deps=$(cat <<EOF
 #   github.com/cosmos/cosmos-sdk
 #   github.com/cosmos/ibc-go/v8
 #   github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8
 #   github.com/CosmWasm/wasmd
-deps = $(egrep '^\s*github.com/(CosmWasm/wasmd|cosmos/(cosmos-sdk|ibc-go|ibc-apps))' ${base_dir}/go.mod | cut -d ' ' -f 1)
+# EOF
+# )
+deps=$(
+  egrep '^\s*github.com/(cosmos/(cosmos-sdk|ibc-go\/v|ibc-apps\/middle)|CosmWasm/wasmd)' \
+  $base_dir/go.mod | cut -d ' ' -f 1
+)
 
 # Install dependencies
 go mod download $deps
