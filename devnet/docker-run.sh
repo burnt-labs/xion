00e209c01619f86449fd84382f9d4289f1c8eb63
2xburnt <169301814+2xburnt@users.noreply.github.com>
2024-08-13 15:26:55 -0500

    add devnet updates

---
+++ devnet/docker-run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Get host directory
+: ${my_dir:="$(realpath $(dirname $0))"}
+: ${base_dir:="$(dirname $my_dir)"}
+
+# Build Docker Imgae
+docker build $base_dir --tag burnt/xiond:develop
+
+# Prepare volume directory in HOME
+mkdir -p ${HOME}/xiond-devnet
+cp $my_dir/*json ${HOME}/xiond-devnet/
+cp $my_dir/entrypoint.sh ${HOME}/xiond-devnet/
+
+# This is provided as an example, adjust as needed
+exec docker run \
+  --user=root \
+  --workdir=/home/xiond \
+  --volume=${HOME}/xiond-devnet:/home/xiond \
+  --entrypoint=/home/xiond/entrypoint.sh \
+  --env="HOME=/home/xiond" \
+  --publish 1317:1317 \
+  --publish 9090:9090 \
+  --publish 26657:26657 \
+  burnt/xiond:develop xiond start
