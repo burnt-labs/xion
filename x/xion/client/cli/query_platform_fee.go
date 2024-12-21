9554ae0f73585268a4c646159179995943e144bf
2xburnt <169301814+2xburnt@users.noreply.github.com>
2024-12-21 17:57:47 -0600

    squashed v14.1.1: https://github.com/burnt-labs/xion/compare/v14.1.1
    null

--- x/xion/client/cli/query_platform_fee.go
+++ x/xion/client/cli/query_platform_fee.go
@@ -9,10 +9,10 @@ import (
 	"github.com/burnt-labs/xion/x/xion/types"
 )
 
-func CmdPlatformFee() *cobra.Command {
+func CmdPlatformPercentage() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "platform-fee",
-		Short: "Get Platform Fee",
+		Use:   "platform-percentage",
+		Short: "Get Platform Percentage",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -24,6 +24,34 @@ func CmdPlatformFee() *cobra.Command {
 			params := &types.QueryPlatformPercentageRequest{}
 
 			res, err := queryClient.PlatformPercentage(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdPlatformMinimum() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "platform-minimum",
+		Short: "Get Platform Minimum",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryPlatformMinimumRequest{}
+
+			res, err := queryClient.PlatformMinimum(cmd.Context(), params)
 			if err != nil {
 				return err
 			}
