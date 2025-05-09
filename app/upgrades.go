8fe719d6eed6aea79ba1d558f4a694fc555c0604
2xburnt <169301814+2xburnt@users.noreply.github.com>
2025-05-09 17:12:32 -0500

    add feeabs epochs to upgrade

--- app/upgrades.go
+++ app/upgrades.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	feeabstypes "github.com/osmosis-labs/fee-abstraction/v8/x/feeabs/types"
 )
 
 const UpgradeName = "v19"
@@ -21,65 +22,123 @@ func (app *WasmApp) RegisterUpgradeHandlers() {
 	if err != nil {
 		panic(fmt.Sprintf("failed to read upgrade info from disk %s", err))
 	}
-	app.WrapSetUpgradeHandler(UpgradeName)
+	
+	app.Logger().Info("setting upgrade handler", "name", UpgradeName)
+	app.UpgradeKeeper.SetUpgradeHandler(UpgradeName, app.NextUpgradeHandler)
 
 	if upgradeInfo.Name == UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{}
-
-		app.Logger().Info("setting upgrade store loaders")
-		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 		app.Logger().Info("upgrade info", "name", upgradeInfo.Name, "height", upgradeInfo.Height)
+		app.NextStoreLoader(upgradeInfo)
 	}
 }
 
-func (app *WasmApp) WrapSetUpgradeHandler(upgradeName string) {
-	app.Logger().Info("setting upgrade handler", "name", upgradeName)
-	app.UpgradeKeeper.SetUpgradeHandler(
-		upgradeName,
-		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (vm module.VersionMap, err error) {
-			sdkCtx := sdktypes.UnwrapSDKContext(ctx)
-			sdkCtx.Logger().Info("running module migrations", "name", plan.Name)
+// VersionStoreLoader is the store loader that is called during the upgrade process.
+func (app *WasmApp) NextStoreLoader(upgradeInfo upgradetypes.Plan) (err error) {
+	app.Logger().Info("setting upgrade store loaders")
+	storeUpgrades := storetypes.StoreUpgrades{
+		// Added:  []string{""},
+		// Deleted: []string{""},
+	}
+	storeLoader := upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades)
+	app.SetStoreLoader(storeLoader)
+	return nil
+}
 
-			minCommission := math.LegacyMustNewDecFromStr("0.05")
-			stakingParams, err := app.StakingKeeper.GetParams(ctx)
-			if err != nil {
-				panic(fmt.Sprintf("failed to get staking params %s", err))
-			}
-			stakingParams.MinCommissionRate = minCommission
-			err = app.StakingKeeper.SetParams(ctx, stakingParams)
-			if err != nil {
-				panic(fmt.Sprintf("failed to set staking params %s", err))
-			}
+// NextUpgradeHandler is the upgrade handler that is called during the upgrade process.
+func (app *WasmApp) NextUpgradeHandler(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (vm module.VersionMap, err error) {
+	sdkCtx := sdktypes.UnwrapSDKContext(ctx)
+	sdkCtx.Logger().Info("running module migrations", "name", plan.Name)
 
-			err = app.StakingKeeper.IterateValidators(ctx, func(_ int64, validator stakingtypes.ValidatorI) (stop bool) {
-				if validator.GetCommission().LT(minCommission) {
-					val := validator.(stakingtypes.Validator)
-					_, err = app.StakingKeeper.UpdateValidatorCommission(ctx, val, minCommission)
-					if err != nil {
-						return true
-					}
-				}
-				return false
-			})
-			if err != nil {
-				panic(fmt.Sprintf("failed to update validator commission %s", err))
-			}
+	// Set the new parameters for mint and staking
+	if err := app.V19StakingForceMinimumCommission(ctx, plan); err != nil {
+		panic(fmt.Sprintf("failed set minimum commissions: %s", err))
+	}
 
-			// Retrieve xion mint params
-			p, err := app.XionKeeper.XionLegacyMintKeeper.GetParams(sdkCtx)
-			if err != nil {
-				panic(fmt.Sprintf("failed to retrieve params from legacy Xion Mint: %s", err))
-			}
-			// Prepare subspace for mint module migration
-			mintSubspace := app.GetSubspace(minttypes.ModuleName).WithKeyTable(minttypes.ParamKeyTable()) //nolint:staticcheck
-			mintSubspace.SetParamSet(sdkCtx, &p)
+	// Set the new parameters for mint
+	if err := app.V19MintParamsChange(sdkCtx, plan); err != nil {
+		panic(fmt.Sprintf("failed to run mint module migrations: %s", err))
+	}
 
-			migrations, err := app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
-			if err != nil {
-				panic(fmt.Sprintf("failed to run migrations: %s", err))
-			}
+	// Add query and swap epochs to the feeabs module
+	if err := app.V19FeeabsEpochAdd(sdkCtx, plan); err != nil {
+		panic(fmt.Sprintf("failed to run feeabs module epoch additions: %s", err))
+	}
 
-			return migrations, err
-		},
-	)
+	// Run the migrations for all modules
+	migrations, err := app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+	if err != nil {
+		panic(fmt.Sprintf("failed to run migrations: %s", err))
+	}
+
+	return migrations, err
+}
+
+// V19StakingParamsChange is a migration function that sets the minimum commission rate for validators to 0.05
+func (app *WasmApp) V19StakingForceMinimumCommission(ctx context.Context, plan upgradetypes.Plan) (err error) {
+	// Get Staking params
+	stakingParams, err := app.StakingKeeper.GetParams(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get staking params %s", err)
+	}
+
+	// Set the minimum commission rate to 0.05
+	minCommission := math.LegacyMustNewDecFromStr("0.05")
+	stakingParams.MinCommissionRate = minCommission
+	err = app.StakingKeeper.SetParams(ctx, stakingParams)
+	if err != nil {
+		return fmt.Errorf("failed to set staking params %s", err)
+	}
+
+	// Iterate over all validators and update their commission rate if it's less than 0.05
+	err = app.StakingKeeper.IterateValidators(ctx, func(_ int64, validator stakingtypes.ValidatorI) (stop bool) {
+		if validator.GetCommission().LT(minCommission) {
+			val := validator.(stakingtypes.Validator)
+			_, err = app.StakingKeeper.UpdateValidatorCommission(ctx, val, minCommission)
+			if err != nil {
+				return true
+			}
+		}
+		return false
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to update validator commission %s", err)
+	}
+	return nil
+}
+
+// V19MintUpgrade is a migration function that sets the minting parameters for the xion mint module
+func (app *WasmApp) V19MintParamsChange(sdkCtx sdktypes.Context, plan upgradetypes.Plan) (err error) {
+	sdkCtx.Logger().Info("running mint module migrations", "name", plan.Name)
+
+	// Retrieve xion mint params
+	p, err := app.XionKeeper.XionLegacyMintKeeper.GetParams(sdkCtx)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve params from legacy Xion Mint: %s", err)
+	}
+	// Prepare subspace for mint module migration
+	mintSubspace := app.GetSubspace(minttypes.ModuleName).WithKeyTable(minttypes.ParamKeyTable()) //nolint:staticcheck
+	mintSubspace.SetParamSet(sdkCtx, &p)
+
+	return nil
+}
+
+// V19FeeabsEpochAdd adds the query and swap epochs to the feeabs module
+func (app *WasmApp) V19FeeabsEpochAdd(sdkCtx sdktypes.Context, plan upgradetypes.Plan) (err error) {
+	sdkCtx.Logger().Info("running feeabs module migrations", "name", plan.Name)
+
+	epochs := []feeabstypes.EpochInfo{
+		feeabstypes.NewGenesisEpochInfo(feeabstypes.DefaultQueryEpochIdentifier, feeabstypes.DefaultQueryPeriod), 
+		feeabstypes.NewGenesisEpochInfo(feeabstypes.DefaultSwapEpochIdentifier, feeabstypes.DefaultSwapPeriod),
+	}
+
+	// Set the epochs
+	for _, epoch := range epochs {
+		err := app.FeeAbsKeeper.AddEpochInfo(sdkCtx, epoch)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
