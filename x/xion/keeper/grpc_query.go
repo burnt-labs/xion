579c9e18a4eba2c963aff7081a08e6d62b53db37
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2025-08-08 16:25:24 -0400

    catch panic if malformed credentials are used

--- x/xion/keeper/grpc_query.go
+++ x/xion/keeper/grpc_query.go
@@ -3,9 +3,14 @@ package keeper
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/url"
 
+	errorsmod "cosmossdk.io/errors"
 	"github.com/go-webauthn/webauthn/protocol"
 	"github.com/go-webauthn/webauthn/webauthn"
 
@@ -16,20 +21,25 @@ import (
 
 var _ types.QueryServer = Keeper{}
 
-func (k Keeper) WebAuthNVerifyRegister(_ context.Context, request *types.QueryWebAuthNVerifyRegisterRequest) (*types.QueryWebAuthNVerifyRegisterResponse, error) {
+func (k Keeper) WebAuthNVerifyRegister(ctx context.Context, request *types.QueryWebAuthNVerifyRegisterRequest) (*types.QueryWebAuthNVerifyRegisterResponse, error) {
 	rp, err := url.Parse(request.Rp)
 	if err != nil {
 		return nil, err
 	}
-
-	data, err := protocol.ParseCredentialCreationResponseBody(bytes.NewReader(request.Data))
-	if err != nil {
-		return nil, err
+	credentials := bytes.NewReader(request.Data)
+	if err := validateAttestation(credentials); err != nil {
+		return nil, errorsmod.Wrapf(types.ErrNoValidWebAuth, err.Error())
 	}
 
-	credential, err := types.VerifyRegistration(rp, request.Addr, request.Challenge, data)
+	data, err := protocol.ParseCredentialCreationResponseBody(credentials)
 	if err != nil {
-		return nil, err
+		return nil, errorsmod.Wrapf(types.ErrNoValidWebAuth, err.Error())
+	}
+
+	sdkCtx := sdktypes.UnwrapSDKContext(ctx) // NOTE: verify this is the same for X nodes
+	credential, err := types.VerifyRegistration(sdkCtx, rp, request.Addr, request.Challenge, data)
+	if err != nil {
+		return nil, errorsmod.Wrapf(types.ErrNoValidWebAuth, err.Error())
 	}
 
 	credentialBz, err := json.Marshal(&credential)
@@ -46,9 +56,14 @@ func (k Keeper) WebAuthNVerifyAuthenticate(_ context.Context, request *types.Que
 		return nil, err
 	}
 
-	data, err := protocol.ParseCredentialRequestResponseBody(bytes.NewReader(request.Data))
+	credentials := bytes.NewReader(request.Data)
+	if err := validateAttestation(credentials); err != nil {
+		return nil, errorsmod.Wrapf(types.ErrNoValidWebAuth, err.Error())
+	}
+
+	data, err := protocol.ParseCredentialRequestResponseBody(credentials)
 	if err != nil {
-		return nil, err
+		return nil, errorsmod.Wrapf(types.ErrNoValidWebAuth, err.Error())
 	}
 
 	var credential webauthn.Credential
@@ -81,4 +96,41 @@ func (k Keeper) PlatformMinimum(ctx context.Context, _ *types.QueryPlatformMinim
 	}
 
 	return &types.QueryPlatformMinimumResponse{Minimums: coins}, nil
+}
+
+func validateAttestation(body io.Reader) error {
+	var ccr protocol.CredentialCreationResponse
+
+	if err := json.NewDecoder(body).Decode(&ccr); err != nil {
+		return err
+	}
+
+	p := &protocol.ParsedAttestationResponse{}
+
+	if err := json.Unmarshal(ccr.AttestationResponse.ClientDataJSON, &p.CollectedClientData); err != nil {
+		return err
+	}
+	rawAuthData := p.AttestationObject.RawAuthData
+	a := p.AttestationObject.AuthData
+
+	minAuthDataLength := 37
+	if minAuthDataLength > len(rawAuthData) {
+		return errors.New(fmt.Sprintf("Expected data greater than %d bytes. Got %d bytes", minAuthDataLength, len(rawAuthData)))
+	}
+
+	a.RPIDHash = rawAuthData[:32]
+	a.Flags = protocol.AuthenticatorFlags(rawAuthData[32])
+	a.Counter = binary.BigEndian.Uint32(rawAuthData[33:37])
+
+	remaining := len(rawAuthData) - minAuthDataLength
+	if a.Flags.HasExtensions() {
+		if remaining != 0 {
+			if len(rawAuthData)-remaining > len(rawAuthData) {
+				return errors.New(fmt.Sprint("Raw Auth Data seems to be malformed"))
+			}
+			a.ExtData = rawAuthData[len(rawAuthData)-remaining:]
+			remaining -= len(a.ExtData)
+		}
+	}
+	return nil
 }
