2f4e9aef2ee8a0f17135e5bee980ca64b918b25a
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2025-08-08 16:24:59 -0400

    use block timestamp for webauth

--- x/xion/types/webauthn.go
+++ x/xion/types/webauthn.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"bytes"
 	"net/url"
 
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/go-webauthn/webauthn/protocol"
 	"github.com/go-webauthn/webauthn/webauthn"
 )
@@ -34,7 +36,7 @@ func (s SmartContractUser) WebAuthnIcon() string {
 
 var _ webauthn.User = SmartContractUser{}
 
-func VerifyRegistration(rp *url.URL, contractAddr string, challenge string, credentialCreationData *protocol.ParsedCredentialCreationData) (*webauthn.Credential, error) {
+func VerifyRegistration(ctx sdktypes.Context, rp *url.URL, contractAddr string, challenge string, credentialCreationData *protocol.ParsedCredentialCreationData) (*webauthn.Credential, error) {
 	config := webauthn.Config{
 		RPID:                   rp.Host,
 		RPDisplayName:          rp.String(),
@@ -54,7 +56,7 @@ func VerifyRegistration(rp *url.URL, contractAddr string, challenge string, cred
 		UserVerification: protocol.VerificationPreferred,
 	}
 
-	return webAuthn.CreateCredential(smartContractUser, session, credentialCreationData)
+	return CreateCredential(webAuthn, ctx, smartContractUser, session, credentialCreationData)
 }
 
 func VerifyAuthentication(rp *url.URL, contractAddr string, challenge string, credential *webauthn.Credential, credentialAssertionData *protocol.ParsedCredentialAssertionData) (bool, error) {
@@ -86,4 +88,24 @@ func VerifyAuthentication(rp *url.URL, contractAddr string, challenge string, cr
 	}
 
 	return true, nil
+}
+
+// CreateCredential verifies a parsed response against the user's credentials and session data.
+func CreateCredential(webauth *webauthn.WebAuthn, ctx sdktypes.Context, user webauthn.User, session webauthn.SessionData, parsedResponse *protocol.ParsedCredentialCreationData) (*webauthn.Credential, error) {
+	if !bytes.Equal(user.WebAuthnID(), session.UserID) {
+		return nil, protocol.ErrBadRequest.WithDetails("ID mismatch for User and Session")
+	}
+
+	if !session.Expires.IsZero() && session.Expires.Before(ctx.BlockTime()) {
+		return nil, protocol.ErrBadRequest.WithDetails("Session has Expired")
+	}
+
+	shouldVerifyUser := session.UserVerification == protocol.VerificationRequired
+
+	invalidErr := parsedResponse.Verify(session.Challenge, shouldVerifyUser, webauth.Config.RPID, webauth.Config.RPOrigins)
+	if invalidErr != nil {
+		return nil, invalidErr
+	}
+
+	return webauthn.MakeNewCredential(parsedResponse)
 }
