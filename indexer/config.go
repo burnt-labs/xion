f4ed8cbb12ea87e9320d97cf4fc2eaa62af06e87
jhernandezb <contact@jhernandez.me>
2025-08-15 13:28:03 -0600

    check error

--- indexer/config.go
+++ indexer/config.go
@@ -1,5 +1,9 @@
 package indexer
 
+const (
+	FlagIndexerEnabled = "indexer-enabled"
+)
+
 // Config defines the indexer configuration.
 type Config struct {
 	Enabled bool `mapstructure:"enabled" json:"enabled"`
